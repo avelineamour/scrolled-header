b71f69dc7b1f6219bac70ff6f707ba314587b462
Razelle McCarrick <razelle@satyaorthopedic.com>
2018-10-22 18:19:15 -0400

    first upload, scrolling header complete

--- main.js
+++ main.js
@@ -4,12 +4,12 @@ $(window).scroll(function(){
   const hero = $(".hero");
   const nav = $("nav")
 
-  const html = "<nav><a href='#'>About</a><a href='#'>Resumé</a><a href='#'>Portfolio</a><a href='#'>Blog</a><a href='#'>Home</a><a href='#'>Contact</a></nav>"
+  const scrolledHeader = "<nav><a href='#'>Home</a><a href='#'>Resumé</a><a href='#'>Portfolio</a><a href='#'>About</a><a href='#'>Blog</a><a href='#'>Contact</a></nav>"
 
   const unscrolledHeader = "<nav><div class='side-nav left-nav'><a href='#'>About</a><a href='#'>Resumé</a><a href='#'>Portfolio</a></div><div class='side-nav'><h3>full stack web developer & designer</h3></div><div class='side-nav right-nav'><a href='#'>Blog</a><a href='#'>Home</a><a href='#'>Contact</a></div></nav>"
 
   if (scroll >= 300) {
-    header.html(html)
+    header.html(scrolledHeader)
     header.addClass('fixed') 
     $("nav").addClass("scrolled-nav")
     hero.addClass("shift-hero");
