07fd7a4b50aaf5adb9e84297c90d80892749cdaf
Razelle McCarrick <razelle@satyaorthopedic.com>
2018-10-22 21:30:19 -0400

    added responsive hamburger nav w/js from scratch

--- main.js
+++ main.js
@@ -1,25 +1,47 @@
-$(window).scroll(function(){
-  const header = $('.header');
-  const scroll = $(window).scrollTop();
-  const hero = $(".hero");
-  const nav = $("nav")
+$(function(){
 
-  const scrolledHeader = "<nav><a href='#'>Home</a><a href='#'>Resumé</a><a href='#'>Portfolio</a><a href='#'>About</a><a href='#'>Blog</a><a href='#'>Contact</a></nav>"
+  const responsiveNav = "<nav><a href='#'>Home</a><a href='#'>Resumé</a><a href='#'>Portfolio</a><a href='#'>About</a><a href='#'>Blog</a><a href='#'>Contact</a><br></nav>"
 
-  const unscrolledHeader = "<nav><div class='side-nav left-nav'><a href='#'>About</a><a href='#'>Resumé</a><a href='#'>Portfolio</a></div><div class='side-nav'><h3>full stack web developer & designer</h3></div><div class='side-nav right-nav'><a href='#'>Blog</a><a href='#'>Home</a><a href='#'>Contact</a></div></nav>"
+  const menu = $("#hamburger")
+  const menuDiv = $(".responsive-menu")
 
-  if (scroll >= 300) {
-    header.html(scrolledHeader)
-    header.addClass('fixed') 
-    $("nav").addClass("scrolled-nav")
-    hero.addClass("shift-hero");
+  menuDiv.empty()
+
+  menu.click(function(){
+    if (menuDiv.html() === "") {
+      menuDiv.addClass("responsive-menu-toggled")
+      menuDiv.html(responsiveNav)
+    }
+    else {
+      menuDiv.removeClass("responsive-menu-toggled")
+      menuDiv.empty()
+    }
+  })
+
+  $(window).scroll(function(){
+    const header = $('.header');
+    const scroll = $(window).scrollTop();
+    const hero = $(".hero");
+    const nav = $("nav")
+  
+    const scrolledHeader = "<a href='#'>Home</a><a href='#'>Resumé</a><a href='#'>Portfolio</a><a href='#'>About</a><a href='#'>Blog</a><a href='#'>Contact</a>"
+  
+    const unscrolledHeader = "<div class='side-nav left-nav'><a href='#'>About</a><a href='#'>Resumé</a><a href='#'>Portfolio</a></div><div class='side-nav'><h3>full stack web developer<br>&<br>designer</h3></div><div class='side-nav right-nav'><a href='#'>Blog</a><a href='#'>Home</a><a href='#'>Contact</a></div>"
+  
+    if (scroll >= 300 && $(window).width() > 768) {
+      nav.html(scrolledHeader)
+      header.addClass('fixed') 
+      $("nav").addClass("scrolled-nav")
+      hero.addClass("shift-hero");
+    }
+    else if (scroll <= 300 && $(window).width() > 768 ) {
+      header.removeClass('fixed');
+      hero.removeClass("shift-hero");
+      nav.html(unscrolledHeader)
+    
   }
-  else {
-    header.removeClass('fixed');
-    hero.removeClass("shift-hero");
-    header.html(unscrolledHeader)
-}
-});
+  });
+})
 
 
 
